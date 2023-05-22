79b2695cd26512b80c5fe3eeebdeb7faa4e59136
awk <awk@awk.space>
2023-05-22 12:57:11 -0400

    Fix pagination

--- generate.py
+++ generate.py
@@ -4,53 +4,58 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-with open('base.xml', 'r') as base:
-    feed = BeautifulSoup(base.read(), 'xml')
+with open("base.xml", "r") as base:
+    feed = BeautifulSoup(base.read(), "xml")
 
-channel = feed.find('channel')
+channel = feed.find("channel")
 
-archive_page = '/archive'
+archive_page = "/archive"
 
 while True:
+    print(f"Fetching {archive_page}")
 
-    request = requests.get('https://www.thisamericanlife.org' + archive_page)
-    archive = BeautifulSoup(request.content, 'html.parser')
+    request = requests.get("https://www.thisamericanlife.org" + archive_page)
+    if request.headers["Content-Type"] == "application/json":
+        content = request.json()["html"]
+    else:
+        content = request.content
+    archive = BeautifulSoup(content, "html.parser")
 
-    for episode_page in archive.select('header > a.goto-episode'):
-
-        full_url = 'https://www.thisamericanlife.org' + episode_page['href']
+    for episode_page in archive.select("header > a.goto-episode"):
+        full_url = "https://www.thisamericanlife.org" + episode_page["href"]
         request = requests.get(full_url)
-        episode = BeautifulSoup(request.content, 'html.parser')
-        player_data = episode.select_one('script#playlist-data').string
+        episode = BeautifulSoup(request.content, "html.parser")
+        player_data = episode.select_one("script#playlist-data").string
         player_data = json.loads(player_data)
 
-        if 'audio' not in player_data:
+        if "audio" not in player_data:
             continue
 
-        if '/promos/' in player_data['audio']:
-            player_data['title'] += ' (Promo)'
+        if "/promos/" in player_data["audio"]:
+            player_data["title"] += " (Promo)"
 
-        title = feed.new_tag('title')
-        title.string = player_data['title']
+        title = feed.new_tag("title")
+        title.string = player_data["title"]
 
-        link = feed.new_tag('link')
+        link = feed.new_tag("link")
         link.string = full_url
 
         meta_pub_time = episode.select(
-            'meta [property=\'article:published_time\']')[0]
+            "meta [property='article:published_time']"
+        )[0]
 
-        pub_date = feed.new_tag('pubDate')
-        pub_date.string = meta_pub_time['content']
+        pub_date = feed.new_tag("pubDate")
+        pub_date.string = meta_pub_time["content"]
 
-        summary = episode.select('meta [name=\'description\']')
-        description = feed.new_tag('description')
-        description.string = summary[0]['content']
+        summary = episode.select("meta [name='description']")
+        description = feed.new_tag("description")
+        description.string = summary[0]["content"]
 
-        enclosure = feed.new_tag('enclosure')
-        enclosure['url'] = player_data['audio']
-        enclosure['type'] = 'audio/mpeg'
+        enclosure = feed.new_tag("enclosure")
+        enclosure["url"] = player_data["audio"]
+        enclosure["type"] = "audio/mpeg"
 
-        item = feed.new_tag('item')
+        item = feed.new_tag("item")
         item.append(title)
         item.append(link)
         item.append(enclosure)
@@ -59,12 +64,12 @@ while True:
 
         channel.append(item)
 
-    pager = archive.select('a.pager')
+    pager = archive.select("a.pager")
 
     if pager:
-        archive_page = pager[0]['href']
+        archive_page = pager[0]["href"]
     else:
         break
 
-with open('feed.xml', 'w') as out:
+with open("feed.xml", "w") as out:
     out.write(feed.prettify())
